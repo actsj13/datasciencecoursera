5e5654627dcbed028ff779dac7e1c9c572f3f7f3
Arpita Chowdhury <actsj13@gmail.com>
2020-06-14 02:31:08 +0600

    Create complete.R

---
+++ Programming Assignment 1 INSTRUCTIONS: Air Pollution/complete.R
@@ -0,0 +1,24 @@
+complete <- function(directory, id = 1:332){
+    setwd(directory)
+    ID= NULL
+    NumberOfCompleteCase= NULL
+    
+    for(i in id){
+        if(i<10){
+            fileName <- paste(directory,'/00',i,'.csv',sep="")
+            dataOfOneFile <- read.csv(file = fileName)
+        }else if(i<100){
+            fileName <- paste(directory,'/0',i,'.csv',sep="")
+            dataOfOneFile <- read.csv(file = fileName)
+        }else {
+            fileName <- paste(directory,'/',i,'.csv',sep="")
+            dataOfOneFile <- read.csv(file = fileName)
+        }
+        dataWithoutNa <- na.omit(dataOfOneFile)
+        
+        ID = c(ID,i)
+        NumberOfCompleteCase=c(NumberOfCompleteCase,nrow(dataWithoutNa)) 
+    }
+    df <- data.frame("id"=ID,"nobs"=NumberOfCompleteCase)
+    print(df)
+}
